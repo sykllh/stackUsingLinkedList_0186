05ddb8597d481b9c6433e7c7618663e1f41d419c
sykllh <sykllhidmi@gmail.com>
2026-05-15 14:36:33 +0700

    menampilkan semua isi linked list / stack satu per satu.

--- stack.cpp
+++ stack.cpp
@@ -55,5 +55,16 @@ public:
         {
             cout << "List is empty." << endl;
         }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
 
 
